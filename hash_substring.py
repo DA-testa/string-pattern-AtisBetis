400d5440b3d3a727f5c871fce9061ea6096c4d83
221RDB421 <125456897+AtisBetis@users.noreply.github.com>
2023-04-17 12:59:51 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,50 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    mode = input()
+    if mode == 'F':
+        try:
+            with open("./tests/06") as f:
+                pattern, text = f.read().splitlines()
+        except FileNotFoundError:
+            print("Input file not found")
+            return None, None
 
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    elif mode == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+
+    else:
+        print("Invalid mode")
+        return None, None
+
+    return pattern, text
+
+def print_occurrences(occurrences):
+    if occurrences:
+        print(' '.join(map(str, occurrences)))
+    else:
+        print("Pattern not found in text")
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    d = 256
+    q = 101
+    p_len = len(pattern)
+    t_len = len(text)
+    h = pow(d, p_len-1, q)
+    p_hash = 0
+    t_hash = 0
+    for i in range(p_len):
+        p_hash = (d*p_hash + ord(pattern[i])) % q
+        t_hash = (d*t_hash + ord(text[i])) % q
+    occurrences = []
+    for i in range(t_len-p_len+1):
+        if p_hash == t_hash and pattern == text[i:i+p_len]:
+            occurrences.append(i)
+        if i < t_len-p_len:
+            t_hash = (d*(t_hash-ord(text[i])*h) + ord(text[i+p_len])) % q
+    return occurrences
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+    pattern, text = read_input()
+    if pattern and text:
+        occurrences = get_occurrences(pattern, text)
+        print_occurrences(occurrences)
